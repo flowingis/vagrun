01e59445dba4043aa31d2914c15cc1ab3965d490
Simone D'Amico <info@simonedamico.com>
2015-06-18 13:27:40 +0200

    Add CleanUp Command

---
+++ src/Ideato/Vagrun/CleanUpCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Ideato\Vagrun;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Filesystem\Filesystem;
+
+class CleanUpCommand extends Command
+{
+    protected $currentDir;
+
+    /** @var Filesystem */
+    protected $fs;
+
+    protected function configure()
+    {
+        $this
+            ->setName('cleanup')
+            ->setDescription('Remove vagrant template from current directory')
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Remove vagrant template without interaction');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->fs = new Filesystem();
+
+        $this->currentDir = getcwd() . DIRECTORY_SEPARATOR;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $force = $input->getOption('force');
+
+        $filesToRemove = array(
+            $this->currentDir . 'Vagrantfile',
+            $this->currentDir . '.vagrant',
+            $this->currentDir . 'vagrant',
+        );
+
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion(
+            "\nAre you really sure you want to delete all vagrant files? The action cannot be reverted. (Y/n)\n",
+            false
+        );
+
+        if (!$force && !$helper->ask($input, $output, $question)) {
+            $output->writeln('<comment>Clean up action skipped</comment>');
+
+            return;
+        }
+
+        try {
+            $this->fs->remove($filesToRemove);
+
+            $output->writeln("<info>Vagrant template successfully removed</info>\n");
+        } catch (\Exception $e) {
+            $output->writeln(sprintf(
+                "<error>An error occurred while trying to remove vagrant template files</error>\n%s",
+                $e->getMessage()
+            ));
+        }
+    }
+}
