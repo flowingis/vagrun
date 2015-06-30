d4c26cb1da256bcc46fbb86c942bc5f013b82b23
Simone D'Amico <info@simonedamico.com>
2015-06-30 15:09:32 +0200

    travis build

--- bin/build.sh
+++ bin/build.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-./bin/box build -v
+./bin/box build
 
 if git status --porcelain | grep .; then
+    echo "git add vagrun.phar"
     git add vagrun.phar
+    echo "git commit -m '[Release] Released a new version of vagrun.phar'"
     git commit -m "[Release] Released a new version of vagrun.phar"
+    echo "git checkout gh-pages"
     git checkout gh-pages
+    echo "git checkout master vagrun.phar"
     git checkout master vagrun.phar
+    echo "git commit -m '[Release] Released a new version of vagrun.phar'"
     git commit -m "[Release] Released a new version of vagrun.phar"
-    git push deploy master
+    echo "git push deploy gh-pages"
     git push deploy gh-pages
 else
     echo "nothing to commit"
