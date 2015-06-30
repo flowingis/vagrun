dd17e49fa9ba0414abac2b9c93a0756631a897a2
Simone D'Amico <info@simonedamico.com>
2015-06-30 14:59:14 +0200

    Added build travis script

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 ./bin/box build
+
+if git status --porcelain | grep .; then
+    git add vagrun.phar
+    git commit -m "[Release] Released a new version of vagrun.phar"
+    git checkout gh-pages
+    git checkout master vagrun.phar
+    git commit -m "[Release] Released a new version of vagrun.phar"
+    git push deploy master
+    git push deploy gh-pages
+fi
+
