e1a93ac075bac3a69cd9a3d256d155182609478b
Simone D'Amico <info@simonedamico.com>
2015-06-30 17:16:23 +0200

    travis build

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@ echo "./bin/box build"
 ./bin/box build
 echo "cp ./vagrun.phar /tmp/vagrun"
 cp ./vagrun.phar /tmp/vagrun
-echo "/tmp/vagrun"
+echo "cd /tmp/vagrun"
 cd /tmp/vagrun
 
 if git status --porcelain | grep .; then
@@ -12,6 +12,10 @@ if git status --porcelain | grep .; then
     git add vagrun.phar
     echo "git commit -m '[Release] Released a new version of vagrun.phar'"
     git commit -m "[Release] Released a new version of vagrun.phar"
+
+    git remote rm origin
+    git remote add origin git@github.com:ideatosrl/vagrun.git
+
     echo "git push origin gh-pages"
     git push origin gh-pages
 else
