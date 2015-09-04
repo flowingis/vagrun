74ca9bc55e35a231a6b31dfb2092a60633543b9b
fain182 <fain182@gmail.com>
2015-09-04 17:17:09 +0200

    Refactored cleanup command

--- src/Ideato/Vagrun/Command/CleanUpCommand.php
+++ src/Ideato/Vagrun/Command/CleanUpCommand.php
@@ -37,35 +37,42 @@ class CleanUpCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $force = $input->getOption('force');
+        $forceOption = $input->getOption('force');
+        if (!$forceOption) {
+            if (  $this->askConfirmation($input, $output) == false ) {
+                $output->writeln('<comment>Clean up action skipped</comment>');
+                return;
+            }
+        }
 
+        try {
+            $this->removeVagrantFiles();
+            $output->writeln("<info>Vagrant template successfully removed</info>\n");
+        } catch (\Exception $e) {
+            $output->writeln("<error>An error occurred while trying to remove vagrant template files</error>\n");
+            $output->writeln($e->getMessage());
+        }
+    }
+
+    private function removeVagrantFiles()
+    {
         $filesToRemove = array(
-            $this->currentDir.'Vagrantfile',
-            $this->currentDir.'.vagrant',
-            $this->currentDir.'vagrant',
+            $this->currentDir . 'Vagrantfile',
+            $this->currentDir . '.vagrant',
+            $this->currentDir . 'vagrant',
         );
 
+        $this->fs->remove($filesToRemove);
+    }
+
+    private function askConfirmation(InputInterface $input, OutputInterface $output)
+    {
         $helper = $this->getHelper('question');
         $question = new ConfirmationQuestion(
             "\nAre you really sure you want to delete all vagrant files? The action cannot be reverted. (Y/n)\n",
             false
         );
-
-        if (!$force && !$helper->ask($input, $output, $question)) {
-            $output->writeln('<comment>Clean up action skipped</comment>');
-
-            return;
-        }
-
-        try {
-            $this->fs->remove($filesToRemove);
-
-            $output->writeln("<info>Vagrant template successfully removed</info>\n");
-        } catch (\Exception $e) {
-            $output->writeln(sprintf(
-                "<error>An error occurred while trying to remove vagrant template files</error>\n%s",
-                $e->getMessage()
-            ));
-        }
+        $confirmation = $helper->ask($input, $output, $question);
+        return $confirmation;
     }
 }
